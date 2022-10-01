03d4455721512d6538caeef5cc6d3cc5ebb5424e
Xulio Coira <xulioc@gmail.com>
2022-10-01 20:07:09 +0200

    change supabase environment variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import { setupSupabaseHelpers } from '@supabase/auth-helpers-sveltekit';
 import { dev } from '$app/environment';
-import { env } from '$env/dynamic/public';
+// import { env } from '$env/dynamic/public';
 // or use the static env
-// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY, {
+export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
 	persistSession: false,
 	autoRefreshToken: false
 });
