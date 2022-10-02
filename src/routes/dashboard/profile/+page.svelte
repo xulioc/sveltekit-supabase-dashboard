726a1c137cf7bcaaac88c94809b5bcd3b32a8918
Xulio Coira <xulioc@gmail.com>
2022-10-02 17:07:34 +0200

    add user profile wip

---
+++ src/routes/dashboard/profile/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { page } from '$app/stores';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	// console.log(data.profile)
+
+</script>
+
+<div>
+	{data.profile.username}
+</div>
+
+<div>
+	{$page.data.session.user.email}
+</div>
+
+<div>
+	{$page.data.session.user.app_metadata.org} ({$page.data.session.user.app_metadata.role})
+</div>
+
+
+
+
+
+
