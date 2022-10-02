3ba9333dcce12879fef2c5a1bec19e03b5dbeb3d
Xulio Coira <xulioc@gmail.com>
2022-10-02 21:58:43 +0200

    add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,4 +14,19 @@
 	});
 </script>
 
+<svelte:head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J1TGFG0V29"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J1TGFG0V29');
+</script>
+	
+</svelte:head>
+
+
 <slot />
--- src/routes/dashboard/users/+page.server.ts
+++ src/routes/dashboard/users/+page.server.ts
@@ -3,7 +3,6 @@ import { error } from '@sveltejs/kit';
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
 
-    // console.log("FETCHING USERS...");
     const result = await fetch('https://dummyjson.com/users');
     const users = await result.json();
     // console.log(users)
