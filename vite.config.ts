a9238a979e6b7f7615b4deb41b28e2472300097c
Xulio Coira <xulioc@gmail.com>
2023-04-23 14:42:13 +0200

    after pnpm update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
-const config: UserConfig = {
+export default defineConfig({
 	plugins: [sveltekit()],
-
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
@@ -11,21 +10,4 @@ const config: UserConfig = {
 	define: {
 		__VERSION__: JSON.stringify(process.env.npm_package_version),
 	},
-
-	// ALLOW VITE SERVE README.md
-	// https://vitejs.dev/config/server-options.html#server-fs-allow
-	// server: {
-	// 	fs: {
-	// 		allow: [
-	// 			// '/README.md',
-	// 		]
-	// 	}
-	// },
-
-	// PREVENT CHARTS.JS ERROR
-	// ssr: {
-	// 	noExternal: ['chart.js']
-	// }
-};
-
-export default config;
+});
