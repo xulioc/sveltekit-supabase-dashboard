04ff5f9d672dba749b22009aed19fe7d48849da2
Xulio Coira <xulioc@gmail.com>
2023-04-28 22:16:59 +0200

    add refresh button to events

--- src/routes/dashboard/_super/events/+page.svelte
+++ src/routes/dashboard/_super/events/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ActionButton from '$lib/components/dashboard/ActionButton.svelte';
 	import DashboardPage from '$lib/components/dashboard/DashboardPage.svelte';
 	import EventsTable from '$lib/components/dashboard/EventsTable.svelte';
 	import { ListIcon } from 'svelte-feather-icons';
@@ -11,6 +12,9 @@
 <DashboardPage>
 	<span slot="icon"><ListIcon /></span>
 	<span slot="title">Events</span>
+	<span slot="actions" class="flex align-middle gap-1">
+		<ActionButton type="refresh" />
+	</span>
 	<span slot="content" class="w-full">
 		<EventsTable events={data.events} />
 	</span>
