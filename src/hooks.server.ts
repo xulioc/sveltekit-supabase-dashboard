838915026db8acb20fc9d118693d88a593e6bd98
Xulio Coira <xulioc@gmail.com>
2023-04-29 17:48:45 +0200

    prevent logging localhost events

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,79 +5,85 @@ import type { Handle } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.supabase = createSupabaseServerClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-		event
-	});
+    event.locals.supabase = createSupabaseServerClient({
+        supabaseUrl: PUBLIC_SUPABASE_URL,
+        supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+        event
+    });
 
-	/**
-	 * a little helper that is written for convenience so that instead
-	 * of calling `const { data: { session } } = await supabase.auth.getSession()`
-	 * you just call this `await getSession()`
-	 */
-	event.locals.getSession = async () => {
-		const {
-			data: { session }
-		} = await event.locals.supabase.auth.getSession();
-		return session;
-	};
+    /**
+     * a little helper that is written for convenience so that instead
+     * of calling `const { data: { session } } = await supabase.auth.getSession()`
+     * you just call this `await getSession()`
+     */
+    event.locals.getSession = async () => {
+        const {
+            data: { session }
+        } = await event.locals.supabase.auth.getSession();
+        return session;
+    };
 
-	// PROTECT ROUTES
-	const session = await event.locals.getSession();
+    // PROTECT ROUTES
+    const session = await event.locals.getSession();
 
-	if (event.url.pathname.startsWith('/dashboard')) {
-		if (!session) {
-			// the user is not signed in
-			throw redirect(303, '/');
-		}
-	}
+    if (event.url.pathname.startsWith('/dashboard')) {
+        if (!session) {
+            // the user is not signed in
+            throw redirect(303, '/');
+        }
+    }
 
-	if (event.url.pathname.startsWith('/dashboard/_admin')) {
-		if (!imAdmin(session?.user)) {
-			console.log('You are not ADMIN!');
-			throw redirect(303, '/dashboard');
-		}
-	}
+    if (event.url.pathname.startsWith('/dashboard/_admin')) {
+        if (!imAdmin(session?.user)) {
+            console.log('You are not ADMIN!');
+            throw redirect(303, '/dashboard');
+        }
+    }
 
-	if (event.url.pathname.startsWith('/dashboard/_super')) {
-		if (!imSuper(session?.user)) {
-			console.log('You are not SUPER!');
-			throw redirect(303, '/dashboard');
-		}
-	}
+    if (event.url.pathname.startsWith('/dashboard/_super')) {
+        if (!imSuper(session?.user)) {
+            console.log('You are not SUPER!');
+            throw redirect(303, '/dashboard');
+        }
+    }
 
-	const ret = resolve(event, {
-		/**
-		 * There´s an issue with `filterSerializedResponseHeaders` not working when using `sequence`
-		 *
-		 * https://github.com/sveltejs/kit/issues/8061
-		 */
-		filterSerializedResponseHeaders(name) {
-			return name === 'content-range';
-		}
-	});
+    const ret = resolve(event, {
+        /**
+         * There´s an issue with `filterSerializedResponseHeaders` not working when using `sequence`
+         *
+         * https://github.com/sveltejs/kit/issues/8061
+         */
+        filterSerializedResponseHeaders(name) {
+            return name === 'content-range';
+        }
+    });
 
-	// LOG EVENTS HERE
-	// console.log(event)
-	// console.log(event.url)
+    // LOG EVENTS HERE
+    // console.log(event)
+    // console.log(event.url)
 
-	// DISCARD _app events
-	if (event.url.toString().includes('/_app/')) {
-		return ret;
-	}
+    if (event.url.toString().startsWith('http://localhost')) {
+        return ret;
+    }
 
-	// if (event.request.method === 'POST') {
-	//     const form_data = await event.request.formData()
-	//     console.log(form_data)
-	// }
+    // DISCARD _app events
+    if (event.url.toString().includes('/_app/')) {
+        return ret;
+    }
 
-	const res = await event.locals.supabase
-		.from('tracking')
-		.insert([{ user: session?.user, event: event.url }]);
-	if (res.error) {
-		console.log(res.error);
-	}
 
-	return ret;
+
+    // if (event.request.method === 'POST') {
+    //     const form_data = await event.request.formData()
+    //     console.log(form_data)
+    // }
+
+    const res = await event.locals.supabase
+        .from('tracking')
+        .insert([{ user: session?.user, event: event.url }]);
+    if (res.error) {
+        console.log(res.error);
+    }
+
+    return ret;
 };
